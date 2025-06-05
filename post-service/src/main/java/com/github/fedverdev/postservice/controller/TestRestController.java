ad37971a56f9d3d41646534cf3c79649ff6cf248
Fedor Verbitsky <fedia.verbitsky@yandex.ru>
2025-06-06 00:56:07 +0700

    post-service - added public and private test methods

--- post-service/src/main/java/com/github/fedverdev/postservice/controller/TestRestController.java
+++ post-service/src/main/java/com/github/fedverdev/postservice/controller/TestRestController.java
@@ -1,12 +1,18 @@
 package com.github.fedverdev.postservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestRestController {
-    @GetMapping("/test")
+    @GetMapping("/test-public")
     public String test() {
         return "test";
+    }
+
+    @GetMapping("/test-private")
+    public String testPrivate(@RequestHeader("X-User-ID") String xUserId) {
+        return "your user id is " + xUserId;
     }
 }
