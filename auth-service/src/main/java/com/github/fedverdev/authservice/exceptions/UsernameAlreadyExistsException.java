0c2cebd64627473d25d7540be8f6857bb2cc0438
Fedor Verbitsky <fedia.verbitsky@yandex.ru>
2025-06-08 19:35:56 +0700

    auth-service - remove unused import

--- auth-service/src/main/java/com/github/fedverdev/authservice/exceptions/UsernameAlreadyExistsException.java
+++ auth-service/src/main/java/com/github/fedverdev/authservice/exceptions/UsernameAlreadyExistsException.java
@@ -1,8 +1,5 @@
 package com.github.fedverdev.authservice.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
 public class UsernameAlreadyExistsException extends RuntimeException {
     public UsernameAlreadyExistsException(String username) {
         super("Username |" + username + "| already exists");
