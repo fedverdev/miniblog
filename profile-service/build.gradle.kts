8a7d44908e9dff0be2eb74cfc3e11db6099f9e9d
Fedor Verbitsky <fedia.verbitsky@yandex.ru>
2025-06-13 19:06:27 +0700

    profile-service - configured grpc

--- profile-service/build.gradle.kts
+++ profile-service/build.gradle.kts
@@ -4,6 +4,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.1.10"
     id("io.ktor.plugin") version "3.2.0"
+    id("com.google.protobuf") version "0.9.5"
 }
 
 group = "com.github.fedverdev"
@@ -25,4 +26,29 @@ dependencies {
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    /* gRPC */
+    implementation("io.grpc:grpc-netty-shaded:1.73.0")
+    implementation("io.grpc:grpc-protobuf:1.73.0")
+    implementation("io.grpc:grpc-stub:1.73.0")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.0.0"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.0.0-pre2"
+        }
+
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+            }
+        }
+    }
 }
