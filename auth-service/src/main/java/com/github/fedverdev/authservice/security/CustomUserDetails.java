88b9da1847256a4d70f08bd0e855db53903f4b7c
Fedor Verbitsky <fedia.verbitsky@yandex.ru>
2025-06-18 17:50:49 +0700

    auth-service - refactor

---
+++ auth-service/src/main/java/com/github/fedverdev/authservice/security/CustomUserDetails.java
@@ -0,0 +1,35 @@
+package com.github.fedverdev.authservice.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    @Getter
+    private final UUID userId;
+    private final String username;
+    private final String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+}
